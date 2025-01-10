aeae8f194cd74c24c5655f78bf07150a184e750c
sai krishna <saikrishnasprasad2642005@gmail.com>
2025-01-10 14:40:42 +0530

    header and background

---
+++ src/header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './header.css';
+
+function Header() {
+    return (
+        <>
+            <div className="header">
+                <img src="./public/vite.svg" alt="logo" height="100%" />
+                <div className="nav">
+                    <button>Explore</button>|
+                    <button>Stats</button>|
+                    <button>Resources</button>|
+                    {/* <button>Create</button> */}
+                    <button id="login">Connect Wallet</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Header;
